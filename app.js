2a7406f150ad4041f3673f92a0bd5547b30c9caa
Pmookerjee <pmookerjee@gmail.com>
2018-06-24 15:29:49 -0700

    Added functions for posting line item and line item org joins.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var data,
+var origCsvData,
     pIDProject,
     pIDContract,
     pIDCustomer,
@@ -8,6 +8,16 @@ var data,
 
 var sessionID = '';
 
+function checkForError(obj) {
+  return obj.Error;
+}
+
+function failPromise(errors, request) {
+  console.log('Errors: ', errors);
+  fail(`Error occurred while posting to ${request}`)
+}
+
+//parse the csv file into an array of objects, preserving type
 Papa.parseCSV = function(file) {
   return new Promise(function(complete, error) {
     Papa.parse(file, {
@@ -17,6 +27,7 @@ Papa.parseCSV = function(file) {
   });
 };
 
+//make sure the right columns are included 
 function verifyHeaders(data) {
   var headers = data[0];
 
@@ -49,6 +60,7 @@ function verifyHeaders(data) {
 
 }
 
+//make sure all the global columns have the same value
 function verifyExpectedGlobals(data) {
 
     function globalMatch(current, expected) {
@@ -66,7 +78,6 @@ function verifyExpectedGlobals(data) {
             throw new Error('Error: Project, Contract, Customer, and Org values must be consistent!')
           }
     })
-
 }
 
 function createBidItems(data){
@@ -94,35 +105,43 @@ function createBidItems(data){
 }
 
 function authenticate(api) {
-  return new Promise(function(complete, error) {
+  return new Promise(function(complete, fail) {
 
-    let agent = superagent.agent();
+     let agent = superagent.agent();
 
       agent.get(`${api}/1.0/Authenticate/marla/rocinante`)
-        .set('Content-Type', 'application/json')
+        .withCredentials()
         .then(function(res){
           sessionID = res.body.SessionID;
-          complete(res.body.SessionID);
+          complete(sessionID);
         })
   })
 }
 
 function postBidItems(bidItems) {
 
-  return new Promise(function (complete, error) {
+  return new Promise(function (complete, fail) {
 
     var API_URL = 'https://headlightqa.paviasystems.com';
 
     authenticate(API_URL)
       .then(function(cookie) {
          superagent.post(`${API_URL}/1.0/BidItems`)
-          .set('Accept', 'application/json')
-          .withCredentials(true)
-          .set('cookie', cookie)
+          .withCredentials()
+          .auth('Authorization', cookie, {type:'auto'})
+          .set({'Content-Type': 'application/json'})
           .send(bidItems)
           .then(function(res) {
-            console.log(`Posted ${bidItems.length} bid items`)
-            complete(res.body);
+            var errors  = res.body.filter(checkForError); 
+            
+            if(errors.length){
+              console.log('Errors: ', errors);
+              fail('Error occurred while posting bid item org joins')
+            }
+            else {
+              console.log(`Posted ${bidItems.length} bid items`);
+              complete(res.body);
+            }
           })
       })
   }) 
@@ -130,7 +149,7 @@ function postBidItems(bidItems) {
 
 function postBidItemOrgJoins(bidItems) {
 
-  return new Promise(function (complete, error) {
+  return new Promise(function (complete, fail) {
 
       var bidItemOrgJoins = [];
 
@@ -148,23 +167,132 @@ function postBidItemOrgJoins(bidItems) {
       superagent.post(`${API_URL}/1.0/BidItemOrganizationJoins`)
       .set('Accept', 'application/json')
       .withCredentials(true)
-      .set('cookie', sessionID)
+      .auth('Authorization', sessionID, {type:'auto'})
       .send(bidItemOrgJoins)
       .then(function(res) {
-        console.log(`Posted ${bidItemOrgJoins.length} bid item org joins`)
-        complete();
+          var errors  = res.body.filter(checkForError); 
+
+          if(errors.length){
+            console.log('Errors: ', checkForError);
+            fail('Error occurred while posting bid item org joins')
+          }
+          else {
+            console.log(`Posted ${bidItemOrgJoins.length} bid item org joins`);
+            complete(bidItems);
+          }
       })
   }) 
 }
+
+function createLineItems(bidItems){
+
+  return new Promise(function (complete, fail) {
+
+    var bidItemLookup = _.indexBy(bidItems, 'Description');
+    var lineItems = [];
+
+    origCsvData.forEach(function(item) {
+          
+        var name = item['LineItemName'];
+        var tmpCode = item['LineItemCode'];
+
+        var lineItemCode = (tmpCode && tmpCode.length) ? tmpCode : name.match(/^[A-Z]*[-: ]*\d+-?\d*/g);    
+        var guid = 'CSV-' + pIDProject + '-' + lineItemCode;
+
+        lineItems.push({
+          GUIDLineItem: guid,
+          Name: name,
+          Description: item['LineItemDescription'],
+          IDBidItem: bidItemLookup[item['LineItemDescription']].IDBidItem,
+          Units: item['Units'],
+          CostPerUnit: (item['CostPerUnit'] + '').replace('$', ''),
+          ExpectedQuantity: item['ContractQuantity'],
+          ContractQuantity: item['ContractQuantity'],
+          CategoryNumber: item['CategoryNumber'] || '',
+          FundingCategory: item['FundingCategory'] || '',
+          Tax: item['Tax'] || 0.0,
+          IDProject: pIDProject,
+          IDCustomer: pIDCustomer
+        });
+          
+      });
+      complete([bidItems, lineItems]);
+    })
+}
+
+function postLineItems([bidItems, lineItems]) {
+
+  return new Promise(function (complete, fail) {
+
+    var API_URL = 'https://headlightqa.paviasystems.com';
+
+      superagent.post(`${API_URL}/1.0/LineItems`)
+      .withCredentials()
+      .auth('Authorization', sessionID, {type:'auto'})
+      .set({'Content-Type': 'application/json'})
+      .send(lineItems)
+      .then(function(res) {
+          var errors  = res.body.filter(checkForError); 
+
+          if(errors.length){
+            console.log('Errors: ', errors);
+            fail('Error occurred while posting line items')
+          }
+          else {
+            console.log(`Posted ${lineItems.length} line items`);
+            complete([bidItems, res.body]);
+          }
+      })
+  }) 
+}
+
+function postLineItemOrgJoins([bidItems, lineItems]) {
+
+  return new Promise(function (complete, fail) {
+
+      var lineItemOrgJoins = [];
+
+      lineItems.forEach(function(item) {
+        lineItemOrgJoins.push({
+          GUIDLineItemOrganizationJoin: item.GUIDLineItem + '-' + pIDOrganization,
+          IDLineItem: item.IDLineItem,
+          IDOrganization: pIDOrganization,
+          IDCustomer: pIDCustomer
+        })
+      })
+            
+      var API_URL = 'https://headlightqa.paviasystems.com';
+
+      superagent.post(`${API_URL}/1.0/LineItemOrganizationJoins`)
+      .set('Accept', 'application/json')
+      .withCredentials(true)
+      .auth('Authorization', sessionID, {type:'auto'})
+      .send(lineItemOrgJoins)
+      .then(function(res) {
+        var errors  = res.body.filter(checkForError); 
+
+        if(errors.length){
+          console.log('Errors: ', errors);
+          fail('Error occurred while posting line items joins')
+        }
+        else {
+          console.log(`Posted ${lineItemOrgJoins.length} line item org joins`);
+          complete([bidItems, res.body]);
+        }
+      })
+  }) 
+}
+
 
 function handleFileSelect(e) {
   var file = e.target.files[0];
 
   Papa.parseCSV(file)
     .then(function(results) { 
-      console.log('data: ', results); 
+      console.log('Beginning csv import...\ndata: ', results); 
       verifyHeaders(results.data);
-      return results.data;
+      origCsvData = results.data;
+      return origCsvData;
     })
     .then(function(data) {
       verifyExpectedGlobals(data);
@@ -179,8 +307,20 @@ function handleFileSelect(e) {
     .then(function(bidItemResponseData) {
        return postBidItemOrgJoins(bidItemResponseData);
     })
+    .then(function(bidItems) {
+       return createLineItems(bidItems);
+    })
+    .then(function(lineItemRequestData) {
+      return postLineItems(lineItemRequestData);
+    })
+    .then(function(lineItemResponseData) {
+      return postLineItemOrgJoins(lineItemResponseData);
+    })
+    // .then(function() {
+    //   return 
+    // })
     .then(function(){
-      console.log('Done.')
+      console.log('CSV import complete.')
     })
     .catch(function(err) {
       console.error(err)
